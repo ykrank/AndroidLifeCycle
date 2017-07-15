a140aed0a631f04eb7a640148135f6e42a1f03eb
ykrank <ykrank@gmail.com>
2017-07-16 02:55:49 +0800

    library group id to com.github.ykrank

--- demo/src/main/java/me/ykrank/androidlifecycle_demo/RxjavaFragment.java
+++ demo/src/main/java/me/ykrank/androidlifecycle_demo/RxjavaFragment.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.github.ykrank.androidlifecycle.AndroidLifeCycle;
+import com.github.ykrank.androidlifecycle.rxjava2.AndroidRxDispose;
+
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -16,14 +19,16 @@ import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import me.ykrank.androidlifecycle.AndroidLifeCycle;
-import me.ykrank.androidlifecycle.rxjava2.AndroidRxDispose;
 
 /**
  * Test rxjava. you can see log after switch to other fragment or activity
  */
 
 public class RxjavaFragment extends android.app.Fragment {
+
+    private static void l(String msg) {
+        Log.d("RxjavaFragment", msg);
+    }
 
     @Nullable
     @Override
@@ -33,30 +38,32 @@ public class RxjavaFragment extends android.app.Fragment {
         Button button = (Button) view.findViewById(R.id.button1);
         AndroidLifeCycle.bindFragment(button, this);
 
-        Observable.intervalRange(1, 100, 0, 1, TimeUnit.SECONDS, Schedulers.single())
-                .doOnDispose(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        l("Rxjava dispose");
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .to(AndroidRxDispose.withObservable(button))
-                .subscribe(new Consumer<Object>() {
-                    @Override
-                    public void accept(@NonNull Object object) throws Exception {
-                        l(object.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(@NonNull Throwable throwable) throws Exception {
-                        throwable.printStackTrace();
-                    }
-                });
-        return view;
-    }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Observable.intervalRange(1, 100, 0, 1, TimeUnit.SECONDS, Schedulers.single())
+                        .doOnDispose(new Action() {
+                            @Override
+                            public void run() throws Exception {
+                                l("Rxjava dispose");
+                            }
+                        })
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .to(AndroidRxDispose.withObservable(v))
+                        .subscribe(new Consumer<Object>() {
+                            @Override
+                            public void accept(@NonNull Object object) throws Exception {
+                                l(object.toString());
+                            }
+                        }, new Consumer<Throwable>() {
+                            @Override
+                            public void accept(@NonNull Throwable throwable) throws Exception {
+                                throwable.printStackTrace();
+                            }
+                        });
+            }
+        });
 
-    private static void l(String msg) {
-        Log.d("RxjavaFragment", msg);
+        return view;
     }
 }
